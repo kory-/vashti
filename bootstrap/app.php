5aa6ba2dd2120405e48bae3fc4a3f06a87813b63
Lee Jun Kit <mohawk.riceball@gmail.com>
2016-01-04 18:40:02 +0800

    use storage path for optimizations

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
 
+$app->useStoragePathForOptimizations(true);
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
